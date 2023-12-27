e9dc6d1b14f6137afebd8547464b262ca067efc3
Chamod Perera <chamod914@gmail.com>
2023-12-27 22:51:35 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,17 @@ import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>Navbar</nav>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hooBankLogo' className='w-[124px] h-[32px]'/>
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, index) => (
+          <li key={nav.id} className='text-white'>
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}      
+      </ul>
+    </nav>
   )
 }
 
