ba3ced5fce1a3f10e9831b852317645fe57b97ec
Chamod Perera <chamod914@gmail.com>
2023-12-29 00:34:52 +0530

    Update

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import {apple, bill, google} from '../assets'
+import styles, {layout} from '../style'
+
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id='product' className={layout.sectionReverse}>
+      
+      <div className={layout.sectionImgReverse}>
+        <img src={bill}  className='w-[100%] h-[100%] relative z-[5]'/>
+        
+        <div className='white__gradient absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full'/>
+        <div className='pink__gradient absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full'/>
+
+      </div>
+
+      <div className={`${layout.sectionInfo} text-center`}>
+        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden' /> billing & invoicing.</h2>
+
+        <p className={`${styles.paragraph} mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
+      </div>
+    </section>
   )
 }
 
--- src/components/Bussiness.jsx
+++ src/components/Bussiness.jsx
@@ -5,7 +5,7 @@ import styles, {layout} from '../style'
 import Button from './Button'
 
 const FeatureCard = ({icon , title, content, index}) => (
-  <div className={`flex flex-row p-6 rounded-[20px] bg-dimBlue ${index !== features.length -1 ? 'mb-6' : 'mb-0'}`}>
+  <div className={`flex flex-row p-6 rounded-[20px] bg-dimBlue ${index !== features.length -1 ? 'mb-6' : 'mb-0'}` }>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter}`}>
       <img src={icon} className='w-[50%] h-[50%] object-contain'/>
     </div>
