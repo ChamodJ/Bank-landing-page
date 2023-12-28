7f26c53659a032c4d3355a230d93ad3616a078df
Chamod Perera <chamod914@gmail.com>
2023-12-28 13:03:57 +0530

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
+import styles from '../style'
+import { discount, robot } from '../assets'
+
+const Hero = () => (
+    <section id='home' className={`flex md:flex-row flex-col `}>
+
+    </section>
   )
-}
 
 export default Hero
